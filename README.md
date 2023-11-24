322f1378fc49ec7cfa30e6c1d32f492f81e121d4
Rubilmax <rmilon@gmail.com>
2023-11-24 16:40:37 +0100

    feat(scripts): initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# Morpho Blue Deployment
+
+Morpho Blue is a noncustodial lending protocol implemented for the Ethereum Virtual Machine.
+Morpho Blue offers a new trustless primitive with increased efficiency and flexibility compared to existing lending platforms.
+It provides permissionless risk management and permissionless market creation with oracle agnostic pricing.
+It also enables higher collateralization factors, improved interest rates, and lower gas consumption.
+The protocol is designed to be a simple, immutable, and governance-minimized base layer that allows for a wide variety of other layers to be built on top.
+Morpho Blue also offers a convenient developer experience with a singleton implementation, callbacks, free flash loans, and account management features.
+
+## Whitepaper
+
+The protocol is described in detail in the [Morpho Blue Whitepaper](./morpho-blue-whitepaper.pdf).
+
+## Getting Started
+
+> [!IMPORTANT]
+> It is advised to use a dedicated address whose only purpose is to deploy all contracts associated with Blue so that addresses are common across EVM-compatible chains.
+
+- `yarn`
+- Add the network key and its corresponding RPC url to `foundry.toml`
+- `yarn deploy {network} --broadcast` followed with appropriate private key management parameters
+
+For example: `yarn deploy mainnet --broadcast --ledger`
+
+## Licence
+
+All scripts are licensed under [`GPL-2.0-or-later`](./LICENSE).
---
+++ script/Deploy.sol
@@ -0,0 +1,150 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+pragma solidity ^0.8.0;
+
+import {IERC20} from "../lib/forge-std/src/interfaces/IERC20.sol";
+
+import {Morpho, MarketParams, MarketParamsLib, Id} from "../lib/morpho-blue/src/Morpho.sol";
+
+import "../lib/forge-std/src/Script.sol";
+import "../lib/forge-std/src/console2.sol";
+
+/// @dev Warning: keys must be ordered alphabetically.
+struct OracleConfig {
+    address baseFeed1;
+    address baseFeed2;
+    address quoteFeed1;
+    address quoteFeed2;
+    address vault;
+    uint256 vaultConversionSample;
+}
+
+/// @dev Warning: keys must be ordered alphabetically.
+struct MarketConfig {
+    address collateralToken;
+    uint256[] lltvs;
+    address loanToken;
+    string name;
+    OracleConfig oracle;
+}
+
+/// @dev Warning: keys must be ordered alphabetically.
+struct AdaptiveCurveIrmConfig {
+    uint256 adjustmentSpeed;
+    uint256 curveSteepness;
+    uint256 initialRateAtTarget;
+    uint256 targetUtilization;
+}
+
+/// @dev Warning: keys must be ordered alphabetically.
+struct DeployConfig {
+    AdaptiveCurveIrmConfig adaptiveCurveIrm;
+    uint256[] lltvs;
+    MarketConfig[] markets;
+    address owner;
+    bytes32 salt;
+}
+
+contract Deploy is Script {
+    using MarketParamsLib for MarketParams;
+
+    Morpho morpho;
+
+    function run(string memory network) external {
+        DeployConfig memory config = _initConfig(network);
+
+        console2.log("Running deployment script using %s...", msg.sender);
+
+        // Deploy Morpho Blue
+        vm.broadcast();
+        morpho = new Morpho{salt: config.salt}(msg.sender);
+
+        console2.log("Deployed Morpho Blue at: %s", address(morpho));
+
+        // Deploy & enable AdaptiveCurveIrm
+        vm.broadcast();
+        address irm = deployCode(
+            "lib/morpho-blue-irm/out/AdaptiveCurveIrm.sol/AdaptiveCurveIrm.json",
+            abi.encode(
+                address(morpho),
+                config.adaptiveCurveIrm.curveSteepness,
+                config.adaptiveCurveIrm.adjustmentSpeed,
+                config.adaptiveCurveIrm.targetUtilization,
+                config.adaptiveCurveIrm.initialRateAtTarget
+            )
+        );
+
+        console2.log("Deployed AdaptiveCurveIrm at: %s", irm);
+
+        vm.broadcast();
+        morpho.enableIrm(irm);
+
+        // Enable all LLTVs
+        for (uint256 i; i < config.lltvs.length; ++i) {
+            uint256 lltv = config.lltvs[i];
+
+            console2.log("Enabling LLTV %e...", lltv);
+
+            vm.broadcast();
+            morpho.enableLltv(lltv);
+        }
+
+        // Create all markets
+        for (uint256 i; i < config.markets.length; ++i) {
+            MarketConfig memory marketConfig = config.markets[i];
+
+            console2.log("Preparing market [%s]...", marketConfig.name);
+
+            for (uint256 j; j < marketConfig.lltvs.length; ++j) {
+                address oracle;
+
+                // Deploy corresponding ChainlinkOracle
+                if (marketConfig.collateralToken != address(0)) {
+                    uint256 baseTokenDecimals = IERC20(marketConfig.collateralToken).decimals();
+                    uint256 quoteTokenDecimals = IERC20(marketConfig.loanToken).decimals();
+
+                    vm.broadcast();
+                    oracle = deployCode(
+                        "lib/morpho-blue-oracles/out/ChainlinkOracle.sol/ChainlinkOracle.json",
+                        abi.encode(
+                            marketConfig.oracle.vault,
+                            marketConfig.oracle.baseFeed1,
+                            marketConfig.oracle.baseFeed2,
+                            marketConfig.oracle.quoteFeed1,
+                            marketConfig.oracle.quoteFeed2,
+                            marketConfig.oracle.vaultConversionSample,
+                            baseTokenDecimals,
+                            quoteTokenDecimals
+                        )
+                    );
+
+                    console2.log("  Deployed ChainlinkOracle at: %s", oracle);
+                }
+
+                MarketParams memory marketParams = MarketParams({
+                    collateralToken: marketConfig.collateralToken,
+                    loanToken: marketConfig.loanToken,
+                    lltv: marketConfig.lltvs[j],
+                    irm: irm,
+                    oracle: oracle
+                });
+
+                console2.log("  Creating market %s...", vm.toString(Id.unwrap(marketParams.id())));
+
+                vm.broadcast();
+                morpho.createMarket(marketParams);
+            }
+        }
+
+        // Transfer ownership
+        console2.log("Set %s as owner...", config.owner);
+
+        vm.broadcast();
+        morpho.setOwner(config.owner);
+    }
+
+    function _initConfig(string memory network) internal returns (DeployConfig memory) {
+        vm.createSelectFork(vm.rpcUrl(network));
+
+        return abi.decode(vm.parseJson(vm.readFile(string.concat("script/config/", network, ".json"))), (DeployConfig));
+    }
+}
